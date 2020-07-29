d69ee6e9c87440ca640213a3492de471c1194aea
Jenny Gao <gao.jenny9@gmail.com>
2020-07-29 21:59:56 +0000

    connected input module to connection object

--- input.js
+++ input.js
@@ -1,22 +1,35 @@
-const { stdin } = require('process');
+const stdin = process.stdin;
+let connection;
 
 // handle user input
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   handleUserInput();
+  // move();
   return stdin;  
 };
 
-// handling Ctrl+C User Input to exit game
+// handling user input
 const handleUserInput = function() {
   stdin.on('data', (key) => {
+    // exit if Ctrl+C
     if(key === '\u0003') {
       process.exit();
-    };
-  })
+    //handling w, a, s, d movements
+    } else if(key === '\u0077') {
+      connection.write("Move: up");
+    } else if(key === '\u0061') {
+      connection.write("Move: left");
+    } else if(key === '\u0073') {
+      connection.write("Move: down");
+    } else if(key === '\u0064') {
+      connection.write("Move: right");
+    }
+  });
 };
 
 module.exports = { setupInput };
