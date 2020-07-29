319eb9fca8dc6cda336e733141c80d9830656163
Jenny Gao <gao.jenny9@gmail.com>
2020-07-29 23:01:29 +0000

    added constants

---
+++ constants.js
@@ -0,0 +1,28 @@
+const IP = '135.23.222.131';
+const PORT = 50541;
+const w = '\u0077';
+const a = '\u0061';
+const s = '\u0073';
+const d = '\u0064';
+
+
+const msgObj = {
+  '\u0031': "ohi there",
+  '\u0032': "\\(*^ v ^)/",
+  '\u0033': "I'm on it <(` v`)>",
+  '\u0034': "(/'O`)/*.`",
+  '\u0035': "Good to see y'all",
+  '\u0036': "Having too much fun"
+}
+
+module.exports = {
+
+  IP,
+  PORT,
+  w,
+  a,
+  s,
+  d,
+  msgObj
+
+};
